bbda049bc27f75eac71093496777beaa5dd7849c
Venkatesh Nyamagoudar <vnyamagoudar@gmail.com>
2023-01-19 17:10:51 -0500

    Initial Commit

---
+++ MyNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyNotes
+//
+//  Created by Venkatesh Nyamagoudar on 1/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
